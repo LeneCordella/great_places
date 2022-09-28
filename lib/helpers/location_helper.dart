f4703400c60f4a7bd86ff50fc0e6c228ed12f7ba
LeneCordella <lene.cordella@gmail.com>
2022-09-28 20:16:31 -0300

    map preview for current location fixed

--- lib/helpers/location_helper.dart
+++ lib/helpers/location_helper.dart
@@ -1,10 +1,11 @@
-const GOOGLE_API_KEY = 'AIzaSyBURS0ixC8EzEPRQ3y8yKc8q-lUswfYwMI';
+const GOOGLE_API_KEY = 'AIzaSyBkzBG1Cfz0qM4vUx8BXP-bKqPhETpaVNM';
 
 class LocationHelper {
   static String generateLocationPreviewImage({
     double latitude,
     double longitude,
   }) {
-    return 'https://maps.googleapis.com/maps/api/staticmap?center=&$latitude,$longitude&zoom=16&size=600x300&maptype=roadmap &markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:C%7C$latitude,$longitude&key=YOUR_API_KEY&signature=$GOOGLE_API_KEY';
+    return
+    'https://maps.googleapis.com/maps/api/staticmap?center=$latitude,$longitude&zoom=13&size=600x300&maptype=roadmap&markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318&markers=color:red%7Clabel:C%7C40.718217,-73.998284 &key=$GOOGLE_API_KEY';
   }
 }
